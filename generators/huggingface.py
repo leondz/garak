71ada3b189c7def886a779bc1d579a170d4ade68
Leon Derczynski <ld@itu.dk>
2023-05-10 21:42:06 -0700

    suppress token/mask length mismatch by default

--- generators/huggingface.py
+++ generators/huggingface.py
@@ -8,8 +8,12 @@ class HFBaseGenerator:
             'text-generation', 
             model=name, 
             do_sample=do_sample,
-            num_return_sequences=num_return_sequences)
+            num_return_sequences=num_return_sequences,
+            )
 
     def generate(self, prompt):
-        raw_output = self.generator(prompt)
+        raw_output = self.generator(
+            prompt,
+            pad_token_id=self.generator.tokenizer.eos_token_id,
+            )
         return [i['generated_text'] for i in raw_output]
