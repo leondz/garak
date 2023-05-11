0bca7181f7db7d172690b90b895ae0f14001ef5c
Leon Derczynski <leonderczynski@gmail.com>
2023-05-11 11:17:35 -0700

    move name above superclass constructor

--- detectors/toxicity.py
+++ detectors/toxicity.py
@@ -4,7 +4,7 @@ from detectors.base import HFDetector
 
 class ToxicCommentModelDetector(HFDetector):
     def __init__(self):
+        self.name = 'toxic-comment-model'
         super().__init__("martin-ha/toxic-comment-model", "toxic")
         self.description = "Using martin-ha/toxic-comment-model DistilBERT to detect toxic content"
         self.bcp47 = 'en'
-        self.name = 'toxic-comment-model'
