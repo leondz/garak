59e7f67b11dc7475920ee1a24aed1fcc8bf4f853
Leon Derczynski <leonderczynski@gmail.com>
2023-05-16 09:52:11 -0700

    fix bug on model import, clarify generator output typing

--- garak/generators/huggingface.py
+++ garak/generators/huggingface.py
@@ -7,7 +7,7 @@ import warnings
 
 class HFBaseGenerator:
     def __init__(self, name, do_sample=True, num_return_sequences=10):
-        self.fullname, self.name = name, name.split('/')[1]
+        self.fullname, self.name = name, name.split('/')[-1]
         print('loading '+Style.RESET_ALL+Fore.LIGHTMAGENTA_EX+'generator'+Style.RESET_ALL+': HF', name)
         self.generator = pipeline(
             'text-generation', 
@@ -17,14 +17,14 @@ class HFBaseGenerator:
             )
         self.deprefix_prompt = False
 
-    def generate(self, prompts):
+    def generate(self, prompt):
         with warnings.catch_warnings():
             warnings.simplefilter("ignore", category=UserWarning)
             raw_output = self.generator(
-                prompts,
+                prompt,
                 pad_token_id=self.generator.tokenizer.eos_token_id,
                 )
-        generations = [i['generated_text'] for i in raw_output]
+        generations = [i['generated_text'] for i in raw_output] # generator returns 10 outputs by default in __init__
         if not self.deprefix_prompt:
             return generations
         else:
