733fa8ca5645cb2dfb3732233a3d7ba568eb78b2
Leon Derczynski <leonderczynski@gmail.com>
2023-05-17 13:52:06 -0700

    fix typing and string-lowering bugs

--- garak/detectors/base.py
+++ garak/detectors/base.py
@@ -15,13 +15,13 @@ class Detector:
         self.accuracy = None
         print(f'loading {Style.RESET_ALL}{Fore.LIGHTBLUE_EX}detector{Style.RESET_ALL}: {self.name}')
 
-    def detect(self, query):
-        if isinstance(query, str):
-            query = [query]
-        if isinstance(query, list):
-            yield None
-        else:
+    def detect(self, outputlist):
+        if isinstance(outputlist, str):
+            outputlist = [outputlist]
+        if not isinstance(outputlist, list):
             raise TypeError('Can only evaluate str or list')
+        else:
+            return [None] * len(outputlist)
 
 class HFDetector(Detector):
 
@@ -39,12 +39,12 @@ class HFDetector(Detector):
 
         self.graceful_fail = False
 
-    def detect(self, query):
-        if isinstance(query, str):
-            query = [query]
-        if isinstance(query, list):
+    def detect(self, outputlist):
+        if isinstance(outputlist, str):
+            outputlist = [outputlist]
+        if isinstance(outputlist, list):
             try:
-                detector_raw_results = self.detector(query, **self.tokenizer_kwargs)
+                detector_raw_results = self.detector(outputlist, **self.tokenizer_kwargs)
             except Exception as e:
                 if self.graceful_fail:
                     print(e)
@@ -68,26 +68,26 @@ class StringDetector(Detector):
         self.substrings = substrings
         self.matchtype = "str" # str or word
     
-    def detect(self, query, case_sensitive=False):
-        if isinstance(query, str):
-            query = [query]
-        if not isinstance(query, list):
+    def detect(self, outputlist, case_sensitive = False):
+        if isinstance(outputlist, str):
+            outputlist = [outputlist]
+        elif not isinstance(outputlist, list):
             raise TypeError('Can only evaluate str or list')        
 
         detector_results = []
-        for q in query:
+        for output in outputlist:
             match = False
             for s in self.substrings:
                 if self.matchtype == "str":
                     if case_sensitive:
-                        if s in q:
+                        if s in output:
                             match = True
                     else:
-                        if lower(s) in lower(q):
+                        if s.lower() in output.lower():
                             match = True
                 elif self.matchtype == "word":
                     s = re.escape(s)
-                    if re.search(f"\\b{s}\\b", q):
+                    if re.search(f"\\b{s}\\b", output):
                         match = True
                 else:
                     raise ValueError(f"Don't know how to process matchtype: {self.matchtype}")
