e7fd2f3e2472ec002faedcdf6d2c4156ff80b572
Leon <ld@itu.dk>
2023-05-18 12:07:06 -0700

    add cohere generator

---
+++ garak/generators/cohere.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import logging
+import os
+import time
+
+from colorama import Fore, Style
+import cohere  
+
+api_key = os.getenv("COHERE_API_KEY")
+
+from generators.base import Generator
+
+COHERE_GENERATION_LIMIT = 5 # c.f. https://docs.cohere.com/reference/generate 18 may 2023
+
+class CohereGenerator(Generator):
+    def __init__(self, name="command", generations=10):
+        self.name = name
+        self.fullname = f"Cohere {self.name}"
+        self.generations = generations
+
+        self.temperature = 0.750
+        self.max_tokens = 200
+        self.k = 0
+        self.p = 0.75
+        self.preset = None
+        self.frequency_penalty = 0.0
+        self.presence_penalty = 0.0
+        self.stop = []
+
+        print(
+            f"loading {Style.RESET_ALL}{Fore.LIGHTMAGENTA_EX}generator{Style.RESET_ALL}: Cohere:{name}"
+        )
+        logging.info("generator init: {self}")
+
+        self.generator = cohere.Client(api_key)
+
+    def generate(self, prompt):
+        if self.generations > COHERE_GENERATION_LIMIT:
+            self.generations = COHERE_GENERATION_LIMIT
+            logging.debug(f"Cohere generation limit capped at {COHERE_GENERATION_LIMIT}")
+        try:
+            response = self.generator.generate(
+                model=self.name,
+                prompt=prompt,
+                temperature=self.temperature,
+                num_generations=self.generations,
+                max_tokens=self.max_tokens,
+                preset=self.preset,
+                k=self.k,
+                p=self.p,
+                frequency_penalty=self.frequency_penalty,
+                presence_penalty=self.presence_penalty,
+                end_sequences=self.stop,
+            )
+        except:
+            return [] # cohere client handles rate limiting ungracefully
+        return [g.text for g in response]
