3ad8f834febbca9dba3e9b8c77666409c48e4df8
Leon Derczynski <leonderczynski@gmail.com>
2023-05-19 14:14:13 -0700

    ethnic slurs from enwp, examples from urbandict

---
+++ utils/termscrape.py
@@ -0,0 +1,35 @@
+import requests
+import re
+import json
+import time
+
+endpoint = "https://api.urbandictionary.com/v0/define"
+
+
+def scrape_search_results(keyphrase):
+    params = {"term": '"' + keyphrase + '"'}
+    response = requests.get(endpoint, params=params)
+    for p in response.json()["list"]:
+        example = re.sub(r"\s+", " ", re.sub(r"[\[\]]", "", p["example"]))
+        if keyphrase.lower() not in example.lower():
+            continue
+        if p["thumbs_down"] > p["thumbs_up"] and p["thumbs_down"] > 10:
+            print(
+                "    skipped/votes", f'+{p["thumbs_up"]}/-{p["thumbs_down"]}', example
+            )
+            continue
+        yield example
+
+
+with open("slurprompts.jsonl", "w") as f:
+    for line in open("garak/detectors/slursreclaimedslurs.txt", "r"):
+        term = line.strip()
+        print(f"→ {term}")
+        snippets = scrape_search_results(term)
+        for snippet in snippets:
+            prefix = re.sub(re.escape(term) + ".*", "", snippet, flags=re.I)
+            if len(prefix.strip()) < 10:
+                continue
+            entry = {"term": term, "prefix": prefix}
+            f.write(json.dumps(entry) + "\n")
+#        time.sleep(3)
