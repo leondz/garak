33e27069c7a081dc7e67686c3b397c52a97cb252
Leon Derczynski <leonderczynski@gmail.com>
2023-05-24 12:26:30 -0700

    add tests that load all probes, detectors, generators

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import re
+
 from garak import __version__, cli
 
 def test_version_command(capsys):
@@ -8,3 +10,21 @@ def test_version_command(capsys):
     assert "garak" in result.out
     assert f"v{__version__}" in result.out
     assert len(result.out.strip().split("\n")) == 1
+
+def test_probe_list(capsys):
+    cli.main(['--list_probes'])
+    result = capsys.readouterr()
+    for line in result.out.strip().split("\n"):
+        assert re.match(r"^probes.[a-z0-9_]+.[A-Za-z0-9_]+$", line)
+
+def test_detector_list(capsys):
+    cli.main(['--list_detectors'])
+    result = capsys.readouterr()
+    for line in result.out.strip().split("\n"):
+        assert re.match(r"^detectors.[a-z0-9_]+.[A-Za-z0-9_]+$", line)
+
+def test_generator_list(capsys):
+    cli.main(['--list_generators'])
+    result = capsys.readouterr()
+    for line in result.out.strip().split("\n"):
+        assert re.match(r"^generators.[a-z0-9_]+.[A-Za-z0-9_]+$", line)
