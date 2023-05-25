bf1433a733d16de743f0af24184defd54e09f85f
Leon Derczynski <leonderczynski@gmail.com>
2023-05-25 15:06:46 -0700

    do better when model_name is forgotten

--- garak/generators/openai.py
+++ garak/generators/openai.py
@@ -15,8 +15,25 @@ sources:
 * https://platform.openai.com/docs/models/model-endpoint-compatibility
 * https://platform.openai.com/docs/model-index-for-researchers
 """
-completion_models = ('text-davinci-003', 'text-davinci-002', 'text-curie-001', 'text-babbage-001', 'text-ada-001', 'code-davinci-002', 'code-davinci-001', 'davinci-instruct-beta')
-chat_models = ('gpt-4', 'gpt-4-0314', 'gpt-4-32k', 'gpt-4-32k-0314', 'gpt-3.5-turbo', 'gpt-3.5-turbo-0301')
+completion_models = (
+    "text-davinci-003",
+    "text-davinci-002",
+    "text-curie-001",
+    "text-babbage-001",
+    "text-ada-001",
+    "code-davinci-002",
+    "code-davinci-001",
+    "davinci-instruct-beta",
+)
+chat_models = (
+    "gpt-4",
+    "gpt-4-0314",
+    "gpt-4-32k",
+    "gpt-4-32k-0314",
+    "gpt-3.5-turbo",
+    "gpt-3.5-turbo-0301",
+)
+
 
 class OpenAIGenerator(Generator):
     def __init__(self, name, generations=10):
@@ -39,9 +56,17 @@ class OpenAIGenerator(Generator):
             self.generator = openai.Completion
         elif self.name in chat_models:
             self.generator = openai.ChatCompletion
+        elif self.name == "":
+            openai_model_list = sorted([m["id"] for m in openai.Model().list()["data"]])
+            raise ValueError(
+                "Model name is required for OpenAI, use --model_name\n"
+                + "  API returns following available models: ▶️   "
+                + "  ".join(openai_model_list) + "\n"
+                + "  ⚠️  Not all these are text generation models"
+            )
         else:
             raise ValueError(
-                f"No model type defined for {model_name} in generators/openai.py - please add one!"
+                f"No OpenAI API defined for '{self.name}' in generators/openai.py - please add one!"
             )
 
     def generate(self, prompt):
@@ -61,7 +86,7 @@ class OpenAIGenerator(Generator):
         elif self.generator == openai.ChatCompletion:
             response = self.generator.create(
                 model=self.name,
-                messages=[{'role':'user', 'content':prompt}],
+                messages=[{"role": "user", "content": prompt}],
                 temperature=self.temperature,
                 top_p=self.top_p,
                 n=self.generations,
@@ -70,12 +95,11 @@ class OpenAIGenerator(Generator):
                 presence_penalty=self.presence_penalty,
                 frequency_penalty=self.frequency_penalty,
             )
-            return [c["message"]['content'] for c in response["choices"]]
+            return [c["message"]["content"] for c in response["choices"]]
         else:
             raise ValueError(
                 f"Unsupported model at generation time in generators/openai.py - please add a clause!"
             )
-
 
 
 default_class = "OpenAIGenerator"
