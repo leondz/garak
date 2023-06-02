1dbe0cdbe2e854d75151722e3748e460e503e6d2
Leon Derczynski <leonderczynski@gmail.com>
2023-06-02 13:54:09 -0700

    add _call_api method w. backoff, for replicate

--- garak/generators/replicate.py
+++ garak/generators/replicate.py
@@ -3,6 +3,9 @@
 import importlib
 import os
 
+import backoff
+import replicate.exceptions
+import tqdm
 
 import garak._config
 from garak.generators.base import Generator
@@ -30,21 +33,31 @@ class ReplicateGenerator(Generator):
             )
         self.replicate = importlib.import_module("replicate")
 
+    @backoff.on_exception(
+        backoff.fibo, replicate.exceptions.ReplicateError, max_value=70
+    )
+    def _call_api(self, prompt):
+        response_iterator = self.replicate.run(
+            self.name,
+            input={
+                "prompt": prompt,
+                "max_length": self.max_length,
+                "temperature": self.temperature,
+                "top_p": self.top_p,
+                "repetition_penalty": self.repetition_penalty,
+                "seed": self.seed,
+            },
+        )
+        return "".join(response_iterator)
+
     def generate(self, prompt):
         outputs = []
-        for i in range(self.generations):
-            response_iterator = self.replicate.run(
-                self.name,
-                input={
-                    "prompt": prompt,
-                    "max_length": self.max_length,
-                    "temperature": self.temperature,
-                    "top_p": self.top_p,
-                    "repetition_penalty": self.repetition_penalty,
-                    "seed": self.seed,
-                },
-            )
-            outputs.append("".join(response_iterator))
+        generation_iterator = tqdm.tqdm(list(range(self.generations)), leave=False)
+        generation_iterator.set_description(
+            self.fullname[:55]
+        )  # replicate names are long incl. hash
+        for i in generation_iterator:
+            outputs.append(self._call_api(prompt))
         return outputs
 
 
