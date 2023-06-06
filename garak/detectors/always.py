593b91b924bd8de96b28a12b87b279c19f59d5cf
Leon Derczynski <leonderczynski@gmail.com>
2023-06-06 13:57:33 -0700

    always pass/fail detector

---
+++ garak/detectors/always.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from typing import List
+
+from garak.attempt import Attempt
+from garak.detectors.base import Detector
+
+class Fail(Detector):
+    """Always Fail (for testing & prototyping)"""
+
+    def __init__(self):
+        super().__init__()
+
+    def detect(self, attempt: Attempt) -> List[float]:
+        return [1.0] * len(attempt.outputs)
+
+class Pass(Detector):
+    """Always Pass (for testing & prototyping)"""
+
+    def __init__(self):
+        super().__init__()
+
+    def detect(self, attempt: Attempt) -> List[float]:
+        return [0.0] * len(attempt.outputs)
