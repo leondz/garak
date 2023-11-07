f4e03b99b3d26f121fde04eb26ec23693185baf9
Leon Derczynski <leonderczynski@gmail.com>
2023-11-07 11:58:38 +0100

    rename langchain generator to langchainllm, add doc comments

--- garak/generators/langchain.py
+++ garak/generators/langchain.py
@@ -3,15 +3,7 @@
 # SPDX-FileCopyrightText: Portions Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-"""Langchain support
-
-Support for Langchain's LLMs, listed here:
-
-  https://python.langchain.com/docs/integrations/llms/
-
-Calls invoke with the prompt and relays the response. No per-LLM specific
-checking, so make sure the right environment variables are set. 
-Set --model_name to the LLM type required.
+"""LangChain generator support
 """
 
 
@@ -23,7 +15,25 @@ import langchain.llms
 from garak.generators.base import Generator
 
 
-class LangchainGenerator(Generator):
+class LangChainLLMGenerator(Generator):
+    """Class supporting LangChain LLM interfaces
+
+    See LangChain's supported models here,
+      https://python.langchain.com/docs/integrations/llms/
+
+    Calls invoke with the prompt and relays the response. No per-LLM specific
+    checking, so make sure the right environment variables are set.
+
+    Set --model_name to the LLM type required.
+
+    Explicitly, garak delegates the majority of responsibility here:
+
+    * the generator calls invoke() on the LLM, which seems to be the most
+      widely supported method
+    * langchain-relevant environment vars need to be set up there
+    * There's no support for chains, just the langchain LLM interface.
+    """
+
     temperature = 0.750
     k = 0
     p = 0.75
@@ -31,11 +41,11 @@ class LangchainGenerator(Generator):
     frequency_penalty = 0.0
     presence_penalty = 0.0
     stop = []
-    generator_family_name = "Langchain"
+    generator_family_name = "LangChain"
 
     def __init__(self, name, generations=10):
         self.name = name
-        self.fullname = f"Langchain {self.name}"
+        self.fullname = f"LangChain LLM {self.name}"
         self.generations = generations
 
         super().__init__(name, generations=generations)
@@ -49,6 +59,12 @@ class LangchainGenerator(Generator):
         self.generator = llm
 
     def generate(self, prompt):
+        """
+        Continuation generation method for LangChain LLM integrations.
+
+        This calls invoke once per generation; invoke() seems to have the best
+        support across LangChain LLM integrations.
+        """
         outputs = []
         generation_iterator = tqdm.tqdm(list(range(self.generations)), leave=False)
         generation_iterator.set_description(self.fullname[:55])
@@ -57,4 +73,4 @@ class LangchainGenerator(Generator):
         return outputs
 
 
-default_class = "LangchainGenerator"
+default_class = "LangChainLLMGenerator"
