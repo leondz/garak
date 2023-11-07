513eaa1a26c596078b2e3d1593a5412dcf539b10
Leon Derczynski <leonderczynski@gmail.com>
2023-11-07 15:23:53 +0100

    baseline NeMo generator

---
+++ garak/generators/nemo.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+"""NeMo LLM interface"""
+
+import json
+import os
+
+import backoff
+import nemollm
+import tqdm
+
+import garak._config
+from garak.generators.base import Generator
+
+
+class NeMoGenerator(Generator):
+    generator_family_name = "NeMo"
+    temperature = 1
+    top_p = 1.0
+    repetition_penalty = 1
+
+    def __init__(self, name=None, generations=10):
+        self.name = name
+        self.fullname = f"NeMo {self.name}"
+        self.seed = garak._config.seed
+        self.api_host = "https://api.llm.ngc.nvidia.com/v1"
+
+        super().__init__(name, generations=generations)
+
+        self.api_key = os.getenv("NGC_API_KEY", default=None)
+        if self.api_key is None:
+            raise ValueError(
+                'Put the NGC API key in the NGC_API_KEY environment variable (this was empty)\n \
+                e.g.: export NGC_API_TOKEN="xXxXxXxXxXxXxXxXxXxX"'
+            )
+        self.org_id = os.getenv("ORG_ID")
+
+        if self.org_id is None:
+            raise ValueError(
+                'Put your org ID in the ORG_ID environment variable (this was empty)\n \
+                e.g.: export NGC_API_TOKEN="xXxXxXxXxXxXxXxXxXxX"\n \
+                Check "view code" on https://llm.ngc.nvidia.com/playground to see the ID'
+            )
+
+        self.nemo = nemollm.api.NemoLLM(
+            api_host=self.api_host, api_key=self.api_key, org_id=self.org_id
+        )
+
+        if self.name is None:
+            print(json.dumps(self.nemo.list_models(), indent=2))
+            raise ValueError("Please specify a NeMo model - see list above")
+
+    @backoff.on_exception(
+        backoff.fibo,
+        (nemollm.error.ServerSideError, nemollm.error.TooManyRequestsError),
+        max_value=70,
+    )
+    def _call_api(self, prompt):
+        response = self.nemo.generate(
+            model=self.name,
+            prompt=prompt,
+            tokens_to_generate=self.max_tokens,
+            temperature=self.temperature,
+            random_seed=self.seed,
+        )
+        return response
+
+    def generate(self, prompt):
+        outputs = []
+        generation_iterator = tqdm.tqdm(list(range(self.generations)), leave=False)
+        generation_iterator.set_description(
+            self.fullname[:55]
+        )  # replicate names are long incl. hash
+        for i in generation_iterator:
+            outputs.append(self._call_api(prompt))
+        return outputs
+
+
+default_class = "NeMoGenerator"
