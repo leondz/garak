b9d6741e864db2b1c8482183448864661bd1af8c
Erick Galinkin <egalinkin@nvidia.com>
2023-11-09 15:11:45 -0500

    MIT license ref

--- garak/resources/autodan/__init__.py
+++ garak/resources/autodan/__init__.py
@@ -3,7 +3,7 @@
 # SPDX-FileCopyrightText: Portions Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-# AutoDAN implementation lightly modified from https://github.com/SheltonLiu-N/AutoDAN
+# AutoDAN implementation derived from https://github.com/SheltonLiu-N/AutoDAN under MIT license.
 # Paper can be found at: https://arxiv.org/abs/2310.04451
 
 # Resources we want to expose
