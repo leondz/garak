e9db59ee777034a5a2eacdba7a40900e585f5e12
Leon Derczynski <leonderczynski@gmail.com>
2023-11-15 07:56:36 +0100

    add debugging rest server

---
+++ garak/resources/rest/restserv.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+from flask import Flask, request
+
+api = Flask(__name__)
+
+
+@api.route("/endpoint", methods=["GET", "POST"])
+def get_endpoint():
+    print("headers", request.headers)
+    print("values", request.values)
+    print("data", request.data)
+    return "endpoint"
+
+
+if __name__ == "__main__":
+    api.run(debug=True, port=37176)
