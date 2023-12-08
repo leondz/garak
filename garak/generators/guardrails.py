a83996983c81e2ece4c99ed1afa4868502c5bcf3
Leon Derczynski <leonderczynski@gmail.com>
2023-12-08 12:45:31 +0100

    spdx

--- garak/generators/guardrails.py
+++ garak/generators/guardrails.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 """NeMo Guardrails generator."""
 
 from typing import List
