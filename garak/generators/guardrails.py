3bc33030e27b6afe80f17a0963282bcc5bf7839a
Razvan Dinu <rdinu@nvidia.com>
2023-12-08 10:40:43 +0200

    Add NeMoGuardrails generator (WIP).

---
+++ garak/generators/guardrails.py
@@ -0,0 +1,40 @@
+"""NeMo Guardrails generator."""
+
+from typing import List
+
+from garak.generators.base import Generator
+
+
+class NeMoGuardrails(Generator):
+    """Generator wrapper for NeMo Guardrails."""
+
+    supports_multiple_generations = False
+    generator_family_name = "Guardrails"
+
+    def __init__(self, name, generations=1):
+        try:
+            from nemoguardrails import RailsConfig, LLMRails
+            from nemoguardrails.logging.verbose import set_verbose
+        except ImportError:
+            raise Exception("You must first install NeMo Guardrails using `pip install nemoguardrails`.")
+
+        self.name = name
+        self.fullname = f"Guardrails {self.name}"
+
+        # Currently, we use the model_name as the path to the config
+        config = RailsConfig.from_path(self.name)
+        self.rails = LLMRails(config=config)
+
+        super().__init__(name, generations=generations)
+
+    def _call_model(self, prompt: str) -> List[str]:
+        print(f"{prompt}")
+
+        result = self.rails.generate(prompt)
+        print(result)
+        print("--")
+
+        return [result]
+
+
+default_class = "NeMoGuardrails"
