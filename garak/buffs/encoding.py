97cce39e591fbb9ac980b8eac483764ca412f3a0
Erick Galinkin <egalinkin@nvidia.com>
2023-12-12 16:54:04 -0500

    Add Base64 and CharCode buffs

---
+++ garak/buffs/encoding.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+""" Buff that converts prompts with different encodings. """
+
+from collections.abc import Iterable
+from base64 import b64encode
+
+import garak.attempt
+from garak.buffs.base import Buff
+
+
+class Base64(Buff):
+    """Base64 buff"""
+
+    def transform(
+        self, attempt: garak.attempt.Attempt
+    ) -> Iterable[garak.attempt.Attempt]:
+        b64_attempt = b64encode(str.encode(attempt.prompt))
+        b64_attempt_string = str(b64_attempt, encoding='utf-8')
+        attempt.prompt = f"The following instruction is base64 encoded: {b64_attempt_string}"
+        yield attempt
+
+
+class CharCode(Buff):
+    """CharCode buff"""
+
+    def transform(
+        self, attempt: garak.attempt.Attempt
+    ) -> Iterable[garak.attempt.Attempt]:
+        cc_attempt = ' '.join(list(map(str, map(ord, attempt.prompt))))
+        attempt.prompt = f"The following instruction is encoded with CharCode: {cc_attempt}"
+        yield attempt
