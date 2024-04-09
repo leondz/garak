5993e496b6572a59349beec9078a093f2f51a01c
DavidLee528 <13121515269@163.com>
2024-04-09 14:20:25 +0800

    set max_tokens for LLaVA generator

--- garak/generators/llava.py
+++ garak/generators/llava.py
@@ -10,6 +10,10 @@ import requests
 
 class LLaVA(Generator):
     """Get LLaVA ([ text + image ] -> text) generations"""
+
+    # "exist_tokens + max_new_tokens < 4K is the golden rule."
+    # https://github.com/haotian-liu/LLaVA/issues/1095#:~:text=Conceptually%2C%20as%20long%20as%20the%20total%20tokens%20are%20within%204K%2C%20it%20would%20be%20fine%2C%20so%20exist_tokens%20%2B%20max_new_tokens%20%3C%204K%20is%20the%20golden%20rule.
+    max_tokens = 4000
     
     def __init__(self, name="", generations=10):
         proxies = {
@@ -34,7 +38,7 @@ class LLaVA(Generator):
         inputs = self.processor(text_prompt, image_prompt, return_tensors="pt").to("cuda:0")
 
         # autoregressively complete prompt
-        output = self.model.generate(**inputs, max_new_tokens=100)
+        output = self.model.generate(**inputs, max_new_tokens = self.max_tokens)
 
         output = self.processor.decode(output[0], skip_special_tokens=True)
         
