a0d55df3532f0744b911c0c285fc41793e36f776
DavidLee528 <13121515269@163.com>
2024-04-12 14:31:07 +0800

    migrate generator LLaVA from garak/generators/llava.py to garak/generators/huggingface.py

--- garak/generators/huggingface.py
+++ garak/generators/huggingface.py
@@ -24,6 +24,8 @@ import warnings
 
 import backoff
 import torch
+from PIL import Image
+from transformers import LlavaNextProcessor, LlavaNextForConditionalGeneration
 
 from garak import _config
 from garak.generators.base import Generator
@@ -485,5 +487,30 @@ class Model(Generator):
         else:
             return [re.sub("^" + re.escape(prompt), "", i) for i in text_output]
 
+class LLaVA(Generator):
+    """Get LLaVA ([ text + image ] -> text) generations"""
+
+    # "exist_tokens + max_new_tokens < 4K is the golden rule."
+    # https://github.com/haotian-liu/LLaVA/issues/1095#:~:text=Conceptually%2C%20as%20long%20as%20the%20total%20tokens%20are%20within%204K%2C%20it%20would%20be%20fine%2C%20so%20exist_tokens%20%2B%20max_new_tokens%20%3C%204K%20is%20the%20golden%20rule.
+    max_tokens = 4000
+    
+    def __init__(self, name="", generations=10):
+        self.processor = LlavaNextProcessor.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf")
+        self.model = LlavaNextForConditionalGeneration.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf", torch_dtype=torch.float16, low_cpu_mem_usage=True) 
+        self.model.to("cuda:0")
+        
+    def generate(self, prompt: str) -> List[str]:
+        text_prompt = prompt[0]
+        image_prompt = Image.open(prompt[1])
+
+
+        inputs = self.processor(text_prompt, image_prompt, return_tensors="pt").to("cuda:0")
+
+        # autoregressively complete prompt
+        output = self.model.generate(**inputs, max_new_tokens = self.max_tokens)
+
+        output = self.processor.decode(output[0], skip_special_tokens=True)
+        
+        return [output]
 
 default_class = "Pipeline"
