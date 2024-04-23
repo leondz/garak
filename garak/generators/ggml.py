148f0f1380ab482b806601ca782f4bddfa4d860e
Jeffrey Martin <jemartin@nvidia.com>
2024-04-23 13:46:39 -0500

    add tests for `ggml` generator (#618)
    * add tests for `ggml` generator
    
    Signed-off-by: Jeffrey Martin <jemartin@nvidia.com>
    
    * validate instance type in test
    
    Signed-off-by: Jeffrey Martin <jemartin@nvidia.com>
    
    ---------
    
    Signed-off-by: Jeffrey Martin <jemartin@nvidia.com>

--- garak/generators/ggml.py
+++ garak/generators/ggml.py
@@ -21,6 +21,7 @@ from garak import _config
 from garak.generators.base import Generator
 
 GGUF_MAGIC = bytes([0x47, 0x47, 0x55, 0x46])
+ENV_VAR = "GGML_MAIN_PATH"
 
 
 class GgmlGenerator(Generator):
@@ -54,9 +55,9 @@ class GgmlGenerator(Generator):
         }
 
     def __init__(self, name, generations=10):
-        self.path_to_ggml_main = os.getenv("GGML_MAIN_PATH")
+        self.path_to_ggml_main = os.getenv(ENV_VAR)
         if self.path_to_ggml_main is None:
-            raise RuntimeError("Executable not provided by environment GGML_MAIN_PATH")
+            raise RuntimeError(f"Executable not provided by environment {ENV_VAR}")
         if not os.path.isfile(self.path_to_ggml_main):
             raise FileNotFoundError(
                 f"Path provided is not a file: {self.path_to_ggml_main}"
