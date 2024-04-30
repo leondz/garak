d4cd309cdf099632e55e3ded5cab5c202ce3436e
Leon Derczynski <leonderczynski@gmail.com>
2024-04-30 11:53:27 +0200

    add nim docs

--- garak/generators/nim.py
+++ garak/generators/nim.py
@@ -18,13 +18,30 @@ from garak.generators.base import Generator
 
 
 class NVHostedNimGenerator(Generator):
-    """Wrapper for NVIDIA-hosted NIMs. Expects NIM_API_KEY environment variable."""
+    """Wrapper for NVIDIA-hosted NIMs. Expects NIM_API_KEY environment variable.
+
+    Uses the [OpenAI-compatible API](https://docs.nvidia.com/ai-enterprise/nim-llm/latest/openai-api.html)
+    via direct HTTP request.
+
+    To get started with this generator:
+    #. Visit [https://build.nvidia.com/explore/reasoning](build.nvidia.com/explore/reasoning)
+    and find the LLM you'd like to use.
+    #. On the page for the LLM you want to use (e.g. [mixtral-8x7b-instruct](https://build.nvidia.com/mistralai/mixtral-8x7b-instruct)),
+    click "Get API key" key above the code snippet. You may need to create an
+    account. Copy this key.
+    #. In your console, Set the ``NIM_API_KEY`` variable to this API key. On
+    Linux, this might look like ``export NIM_API_KEY="nvapi-xXxXxXx"``.
+    #. Run garak, setting ``--model_name`` to ``nim`` and ``--model_type`` to
+    the name of the model on [build.nvidia.com](https://build.nvidia.com/)
+    - e.g. ``mistralai/mixtral-8x7b-instruct-v0.1``.
+
+    """
 
     supports_multiple_generations = False
     generator_family_name = "NIM"
     temperature = 0.1
     top_p = 0.7
-    top_k = 0 # top_k is hard set to zero as of 24.04.30
+    top_k = 0  # top_k is hard set to zero as of 24.04.30
 
     url = "https://integrate.api.nvidia.com/v1/chat/completions"
 
@@ -101,6 +118,9 @@ class NVHostedNimGenerator(Generator):
         else:
             response_body = response.json()
 
+            # per https://docs.nvidia.com/ai-enterprise/nim-llm/latest/openai-api.html
+            # `n>1` is not supported, so the [0] is safe.
+            #
             return response_body["choices"][0]["message"]["content"]
 
 
