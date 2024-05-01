fe5b8d48d2b89b62c4f92c3908fef224c6d9ee0d
Leon Derczynski <leonderczynski@gmail.com>
2024-05-01 11:03:38 +0200

    support chat modality in attempts

--- garak/attempt.py
+++ garak/attempt.py
@@ -1,5 +1,6 @@
 """Defines the Attempt class, which encapsulates a prompt with metadata and results"""
 
+from typing import Any, List
 import uuid
 
 (
@@ -7,6 +8,8 @@ import uuid
     ATTEMPT_STARTED,
     ATTEMPT_COMPLETE,
 ) = range(3)
+
+roles = {"system", "user", "assistant"}
 
 
 class Attempt:
@@ -33,6 +36,29 @@ class Attempt:
     :type goal: str
     :param seq: Sequence number (starting 0) set in :meth:`garak.probes.base.Probe.probe`, to allow matching individual prompts with lists of answers/targets or other post-hoc ordering and keying
     :type seq: int
+    :param messages: conversation turn histories; list of list of dicts have the format {"role": role, "content": text}, with actor being something like "system", "user", "assistant"
+    :type messages: List(dict)
+
+    Expected use
+    * an attempt tracks a seed prompt and responses to it
+    * there's a 1:1 relationship between attempts and source prompts
+    * attempts track all generations
+    * this means messages tracks many histories, one per generation
+    * for compatibility, setting Attempt.prompt will set just one turn, and this is unpacked later
+      when output is set; we don't know the # generations to expect until some output arrives
+    * to keep alignment, generators need to return aligned lists of length #generations
+
+    Patterns/expectations for Attempt access:
+    .prompt - returns the first user prompt
+    .outputs - returns the most recent model outputs
+    .latest_prompts - returns a list of the latest user prompts
+
+    Patterns/expectations for Attempt setting:
+    .prompt - sets the first prompt, or fails if this has already been done
+    .outputs - sets a new layer of model responses. silently handles expansion of prompt to multiple histories
+    .latest_prompts - adds a new set of user prompts
+
+
     """
 
     def __init__(
@@ -47,33 +73,21 @@ class Attempt:
         detector_results=None,
         goal=None,
         seq=-1,
+        messages=None,
     ) -> None:
         self.uuid = uuid.uuid4()
         self.status = status
-        self.prompt = prompt
+        if prompt is not None:
+            self.prompt = prompt
         self.probe_classname = probe_classname
-        if probe_params is None:
-            self.probe_params = {}
-        else:
-            self.probe_params = probe_params
-        if targets is None:
-            self.targets = []
-        else:
-            self.targets = targets
-        if outputs is None:
-            self.outputs = []
-        else:
-            self.outputs = outputs
-        if notes is None:
-            self.notes = {}
-        else:
-            self.notes = notes
-        if detector_results is None:
-            self.detector_results = {}
-        else:
-            self.detector_results = detector_results
+        self.probe_params = {} if probe_params is None else probe_params
+        self.targets = [] if targets is None else targets
+        # self.outputs = [] if outputs is None else outputs
+        self.notes = {} if notes is None else notes
+        self.detector_results = {} if detector_results is None else detector_results
         self.goal = goal
         self.seq = seq
+        self.messages = [] if messages is None else messages
 
     def as_dict(self) -> dict:
         """Converts the attempt to a dictionary."""
@@ -90,4 +104,118 @@ class Attempt:
             "detector_results": self.detector_results,
             "notes": self.notes,
             "goal": self.goal,
+            "messages": self.messages,
         }
+
+    def __getattribute__(self, name: str) -> Any:
+        """override prompt and outputs access to take from history"""
+        if name == "prompt":
+            if len(self.messages) == 0:  # nothing set
+                return None
+            if isinstance(self.messages[0], dict):  # only initial prompt set
+                return self.messages[0]["content"]
+            if isinstance(
+                self.messages, list
+            ):  # there's initial prompt plus some history
+                return self.messages[0][0]["content"]
+            else:
+                raise ValueError(
+                    "Message history of attempt uuid %s in unexpected state, sorry: "
+                    % str(self.uuid)
+                    + repr(self.messages)
+                )
+
+        elif name == "outputs":
+            if len(self.messages) and isinstance(self.messages[0], list):
+                # work out last_output_turn that was assistant
+                last_output_turn = max(
+                    [
+                        idx
+                        for idx, val in enumerate(self.messages[0])
+                        if val["role"] == "assistant"
+                    ]
+                )
+                # return these (via list compr)
+                return [m[last_output_turn]["content"] for m in self.messages]
+            else:
+                return []
+
+        elif name == "latest_prompts":
+            if len(self.messages) > 1:
+                # work out last_output_turn that was assistant
+                last_output_turn = max(
+                    [
+                        idx
+                        for idx, val in enumerate(self.messages[0])
+                        if val["role"] == "user"
+                    ]
+                )
+                # return these (via list compr)
+                return [m[last_output_turn]["content"] for m in self.messages]
+            else:
+                return (
+                    self.prompt
+                )  # returning a string instead of a list tips us off that generation count is not yet known
+
+        else:
+            return super().__getattribute__(name)
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        """override prompt and outputs access to take from history
+        NB. output elements need to be able to be None
+        """
+        if name == "prompt":
+            assert isinstance(value, str)
+            self.add_first_turn("user", value)
+
+        elif name == "outputs":
+            assert isinstance(value, list)
+            # do we have only one prompt? in which case, let's flesh out messages a bit
+            if len(self.messages) == 1:
+                base_message = dict(self.messages[0])
+                self.messages = [[base_message] for i in range(len(value))]
+            # append each list item to each history, with role:assistant
+            self.add_turns("assistant", value)
+
+        elif name == "latest_prompts":
+            assert isinstance(value, list)
+            self.add_turns("user", value)
+
+        else:
+            return super().__setattr__(name, value)
+
+    def add_first_turn(self, role: str, content: str) -> None:
+        if self.messages != []:
+            raise ValueError(
+                "Cannot set prompt of attempt uuid %s with content already in message history: "
+                % str(self.uuid)
+                + repr(self.messages)
+            )
+        if role in roles:
+            self.messages.append({"role": role, "content": content})
+            return
+        raise ValueError(
+            "Conversation turn role must be one of '%s', got '%s'"
+            % ("'/'".join(roles), role)
+        )
+
+    def add_turns(self, role: str, contents: List[str]) -> None:
+        if len(contents) != len(self.messages):
+            raise ValueError(
+                "Message history misalignment in attempt uuid %s: tried to add %d items to %d message histories"
+                % (str(self.uuid), len(contents), len(self.messages))
+            )
+        if role == "user" and (
+            len(self.messages) == 0 or isinstance(self.messages[0], dict)
+        ):
+            raise ValueError(
+                "can only add a list of user prompts after at least one system generation, so that generations count is known"
+            )
+        if role in roles:
+            for idx, entry in enumerate(contents):
+                self.messages[idx].append({"role": role, "content": entry})
+            return
+        raise ValueError(
+            "Conversation turn role must be one of '%s', got '%s'"
+            % ("'/'".join(roles), role)
+        )
--- tests/test_attempt.py
+++ tests/test_attempt.py
@@ -6,6 +6,7 @@ import json
 import os
 import pytest
 
+import garak.attempt
 import garak.cli
 
 
@@ -36,3 +37,93 @@ def cleanup(request):
             os.remove("_garak_test_attempt_sticky_params.hitlog.jsonl")
 
     request.addfinalizer(remove_reports)
+
+
+def test_turn_taking():
+    a = garak.attempt.Attempt()
+    assert a.messages == [], "Newly constructed attempt should have no message history"
+    assert a.outputs == [], "Newly constructed attempt should have empty outputs"
+    assert a.prompt is None, "Newly constructed attempt should have no prompt"
+    first_prompt = "what is up"
+    a.prompt = first_prompt
+    assert (
+        a.prompt == first_prompt
+    ), "Setting attempt.prompt on new prompt should lead to attempt.prompt returning that prompt string"
+    assert a.messages == [{"role": "user", "content": first_prompt}]
+    assert a.outputs == []
+    first_response = ["not much", "as an ai"]
+    a.outputs = first_response
+    assert a.prompt == first_prompt
+    assert a.messages == [
+        [
+            {"role": "user", "content": first_prompt},
+            {"role": "assistant", "content": first_response[0]},
+        ],
+        [
+            {"role": "user", "content": first_prompt},
+            {"role": "assistant", "content": first_response[1]},
+        ],
+    ]
+    assert a.outputs == first_response
+
+
+def test_history_lengths():
+    a = garak.attempt.Attempt()
+    a.prompt = "sup"
+    assert len(a.messages) == 1, "Attempt with one prompt should have one history"
+    generations = 4
+    a.outputs = ["x"] * generations
+    assert len(a.messages) == generations, "Attempt should expand history automatically"
+    with pytest.raises(ValueError):
+        a.outputs = ["x"] * (generations - 1)
+    with pytest.raises(ValueError):
+        a.outputs = ["x"] * (generations + 1)
+    new_prompt_text = "y"
+    a.latest_prompts = [new_prompt_text] * generations
+    assert len(a.messages) == generations, "History should track all generations"
+    assert len(a.messages[0]) == 3, "Three turns so far"
+    assert (
+        len(a.latest_prompts) == generations
+    ), "Should be correct number of latest prompts"
+    assert (
+        a.latest_prompts[0] == new_prompt_text
+    ), "latest_prompts should be tracking latest addition"
+
+
+def test_illegal_ops():
+    a = garak.attempt.Attempt()
+    with pytest.raises(ValueError):
+        a.latest_prompts = [
+            "a"
+        ]  # shouldn't be able to set latest_prompts until the generations count is known, from outputs()
+
+    a = garak.attempt.Attempt()
+    a.prompt = "prompts"
+    with pytest.raises(ValueError):
+        a.latest_prompts = [
+            "a"
+        ]  # shouldn't be able to set latest_prompts until the generations count is known, from outputs()
+
+    a = garak.attempt.Attempt()
+    a.prompt = "prompt"
+    with pytest.raises(ValueError):
+        a.prompt = "shouldn't be able to set initial prompt twice"
+
+    a = garak.attempt.Attempt()
+    a.prompt = "prompt"
+    a.outputs = ["output"]
+    with pytest.raises(ValueError):
+        a.prompt = "shouldn't be able to set initial prompt after output turned up"
+
+    a = garak.attempt.Attempt()
+    a.prompt = "prompt"
+    a.outputs = ["output"]
+    with pytest.raises(ValueError):
+        a.latest_prompts = [
+            "reply1",
+            "reply2",
+        ]  # latest_prompts size must match outputs size
+
+    a = garak.attempt.Attempt()
+    with pytest.raises(ValueError):
+        a.outputs = ["oh no"]  # shouldn't be able to set outputs until prompt is there
