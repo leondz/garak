f49718d9dfa72ab54b2624baacfece8be892b0b9
Leon Derczynski <leonderczynski@gmail.com>
2024-05-06 12:14:55 +0200

    further prune nim implementation

--- garak/generators/nim.py
+++ garak/generators/nim.py
@@ -5,24 +5,14 @@
 
 """NVIDIA Inference Microservice LLM interface"""
 
-import json
-import logging
-import os
-from typing import List, Union
-
-import requests
+from typing import List
 
 import openai
-import backoff
 
-from garak import _config
 from garak.generators.openai import OpenAICompatible
 
-chat_models = ("meta/llama3-70b-instruct",)
-completion_models = ()
 
-
-class NVHostedNimGenerator(OpenAICompatible):
+class NVOpenAIChat(OpenAICompatible):
     """Wrapper for NVIDIA-hosted NIMs. Expects NIM_API_KEY environment variable.
 
     Uses the [OpenAI-compatible API](https://docs.nvidia.com/ai-enterprise/nim-llm/latest/openai-api.html)
@@ -44,34 +34,53 @@ class NVHostedNimGenerator(OpenAICompatible):
 
     # per https://docs.nvidia.com/ai-enterprise/nim-llm/latest/openai-api.html
     # 2024.05.02, `n>1` is not supported
-    ENV_VAR="NIM_API_KEY"
+    ENV_VAR = "NIM_API_KEY"
     supports_multiple_generations = False
     generator_family_name = "NIM"
     temperature = 0.1
     top_p = 0.7
     top_k = 0  # top_k is hard set to zero as of 24.04.30
 
-    url = "https://integrate.api.nvidia.com/v1"
+    url = "https://integrate.api.nvidia.com/v1/chat/completions"
 
     timeout = 60
 
     def _load_client(self):
         self.client = openai.OpenAI(base_url=self.url, api_key=self.api_key)
-
-        if self.name in completion_models:
-            self.generator = self.client.completions
-        elif self.name in chat_models:
-            self.generator = self.client.chat.completions
-        elif "-".join(self.name.split("-")[:-1]) in chat_models and re.match(
-            r"^.+-[01][0-9][0-3][0-9]$", self.name
-        ):  # handle model names -MMDDish suffix
-            self.generator = self.client.completions
-        else:
-            self.generator = self.client.completions  # fallback as chat for `unknown`
+        self.generator = self.client.chat.completions
 
     def _clear_client(self):
         self.generator = None
         self.client = None
 
 
-default_class = "NVHostedNimGenerator"
+class NVOpenAICcompletion(NVOpenAIChat):
+    """Wrapper for NVIDIA-hosted NIMs. Expects NIM_API_KEY environment variable.
+
+    Uses the [OpenAI-compatible API](https://docs.nvidia.com/ai-enterprise/nim-llm/latest/openai-api.html)
+    via direct HTTP request.
+
+    This generator supports only ``completion`` and NOT ``chat``-format models.
+
+    To get started with this generator:
+    #. Visit [build.nvidia.com/explore/reasoning](build.nvidia.com/explore/reasoning)
+    and find the LLM you'd like to use.
+    #. On the page for the LLM you want to use (e.g. [mixtral-8x7b-instruct](https://build.nvidia.com/mistralai/mixtral-8x7b-instruct)),
+    click "Get API key" key above the code snippet. You may need to create an
+    account. Copy this key.
+    #. In your console, Set the ``NIM_API_KEY`` variable to this API key. On
+    Linux, this might look like ``export NIM_API_KEY="nvapi-xXxXxXx"``.
+    #. Run garak, setting ``--model_name`` to ``nim`` and ``--model_type`` to
+    the name of the model on [build.nvidia.com](https://build.nvidia.com/)
+    - e.g. ``mistralai/mixtral-8x7b-instruct-v0.1``.
+
+    """
+
+    url = "https://integrate.api.nvidia.com/v1/completions"
+
+    def _load_client(self):
+        self.client = openai.OpenAI(base_url=self.url, api_key=self.api_key)
+        self.generator = self.client.completions
+
+
+default_class = "NVOpenAIChat"
