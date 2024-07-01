9279639158a389bad24eb07053bb8b5cd8853a86
Leon Derczynski <leonderczynski@gmail.com>
2024-07-01 20:57:00 +0200

    probe: assess 🤗 repos for potentially malicious files (#754)
    * draft probe for scanning file formats
    
    * draft probe for scanning file formats
    
    * accept generators in detector results, allowing 'yield'
    
    * correctly clear up detector result dict in attempt
    
    * relax some constraints around detector detect() return type
    
    * remove dead code
    
    * list can be set successfully from within iteration, generators less so
    
    * add detector for possible pickle file extensions
    
    * add probe, detector, and doc stubs for checking for pickle extensions
    
    * add possiblepickle test, fix multiple yield bug
    
    * add links to docs
    
    * add hub requirement
    
    * rollback
    
    * overwrite openai options
    
    * Revert "overwrite openai options"
    
    This reverts commit c9aab98b07581c2a8d1b6a86e481a7958d28ce20.
    
    Signed-off-by: Jeffrey Martin <jemartin@nvidia.com>
    
    * add deps, prune import
    
    * scan hf repo contents for pickles
    
    * shift responsibility for file fetching to probe, making detector more generic
    
    * check attempt format, check if path is file
    
    * tighten data validation
    
    * add FileIsPickle test for: format; non-pickled data; default protocol pickled data; fixed protocol pickled data
    
    * refactor fileformat detector, add rudimentary executable checking
    
    * refactor fileformat detector, add rudimentary executable checking
    
    * fileformats test executable file excerpts
    
    * add python-magic dep
    
    * fix dep name
    
    * add type sigs, allow format check skipping
    
    * possiblepickledetector is now a filedetector
    
    * rm stray debug line
    
    Co-authored-by: Jeffrey Martin <jemartin@nvidia.com>
    Signed-off-by: Leon Derczynski <leonderczynski@gmail.com>
    
    * log non-isfile() entries in filedetector
    
    * test skipping behaviour for FileDetector
    
    * fix missing import
    
    * detectors can return generators
    
    * move FileDetector over to all_outputs; pause extended testing of all_outputs detection result for FileDetectors
    
    ---------
    
    Signed-off-by: Jeffrey Martin <jemartin@nvidia.com>
    Signed-off-by: Leon Derczynski <leonderczynski@gmail.com>
    Co-authored-by: Jeffrey Martin <jemartin@nvidia.com>

---
+++ tests/detectors/test_detectors_fileformats.py
@@ -0,0 +1,119 @@
+# SPDX-FileCopyrightText: Portions Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+import glob
+from pathlib import Path
+import pickle
+import pytest
+import tempfile
+
+from garak.attempt import Attempt
+import garak.detectors.base
+import garak.detectors.fileformats
+
+
+def test_possiblepickle_init():
+    d = garak.detectors.fileformats.PossiblePickleName()
+    assert isinstance(d, garak.detectors.base.Detector)
+
+
+possible_pickles = [
+    ("config.json", 0.0),
+    ("file.onnx", 0.0),
+    ("file.pth", 0.0),
+    ("model.pkl", 1.0),
+    ("model.pickle", 1.0),
+]
+
+
+@pytest.mark.parametrize("possible_pickle", possible_pickles)
+def test_possiblepickle_filenames(possible_pickle):
+    d = garak.detectors.fileformats.PossiblePickleName()
+    assert d._test_file(possible_pickle[0]) == possible_pickle[1]
+
+
+def test_fileispickled_invalid_format():
+    d = garak.detectors.fileformats.FileIsPickled()
+    plain_attempt = Attempt(prompt="")
+    plain_attempt.outputs = ["a", "b", "c"]
+    with pytest.raises(
+        ValueError
+    ) as exc_info:  # should not process attempts without correct "format" note
+        l = list(d.detect(plain_attempt))
+
+
+def test_fileispickled_valid_format():
+    d = garak.detectors.fileformats.FileIsPickled()
+    file_attempt = Attempt(prompt="")
+    file_attempt.notes["format"] = "local filename"
+    file_attempt.outputs = [""]
+    l = list(d.detect(file_attempt))
+    assert l == []
+
+
+def test_fileispickled_nonpickle():
+    d = garak.detectors.fileformats.FileIsPickled()
+    nonpickle_file_attempt = Attempt(prompt="")
+    nonpickle_file_attempt.notes["format"] = "local filename"
+    with tempfile.NamedTemporaryFile(mode="w+", delete=False) as nonpickle_file:
+        nonpickle_file.write(
+            "Do you know what the sad part is, Odo? I'm a very good tailor."
+        )
+        nonpickle_file.close()
+        nonpickle_file_attempt.outputs = [nonpickle_file.name]
+        l = list(d.detect(nonpickle_file_attempt))
+        assert l == [0.0]
+
+
+def test_fileispickled_pickle_default():
+    d = garak.detectors.fileformats.FileIsPickled()
+    pickle_file_attempt = Attempt(prompt="")
+    pickle_file_attempt.notes["format"] = "local filename"
+    with tempfile.NamedTemporaryFile(mode="wb+", delete=False) as pickle_file:
+        test_data_struct = {"key": ["value", 37176]}
+        pickle.dump(test_data_struct, pickle_file)
+        pickle_file.close()
+        pickle_file_attempt.outputs = [pickle_file.name]
+        l = list(d.detect(pickle_file_attempt))
+        assert l == [1.0]
+
+
+pickle_versions = [0, 1, 2, 3, 4, 5]
+
+
+@pytest.mark.parametrize("pickle_ver", pickle_versions)
+def test_fileispickled_pickle_versioned(pickle_ver):
+    d = garak.detectors.fileformats.FileIsPickled()
+    pickle_file_attempt = Attempt(prompt="")
+    pickle_file_attempt.notes["format"] = "local filename"
+    with tempfile.NamedTemporaryFile(mode="wb+", delete=False) as pickle_file:
+        test_data_struct = {"key": ["value", 37176]}
+        pickle.Pickler(pickle_file, pickle_ver).dump(test_data_struct)
+        pickle_file.close()
+        pickle_file_attempt.outputs = [pickle_file.name]
+        l = list(d.detect(pickle_file_attempt))
+        assert l == [1.0]
+
+
+def test_fileisexectuable_nonexec():
+    exec_attempt = garak.attempt.Attempt(prompt="")
+    exec_attempt.notes["format"] = "local filename"
+    d = garak.detectors.fileformats.FileIsExecutable()
+    with tempfile.NamedTemporaryFile(mode="w+", delete=False) as test_file:
+        test_file.write("I'm a very good tailor.")
+        test_file.close()
+        exec_attempt.outputs = [test_file.name]
+        results = d.detect(exec_attempt)
+        assert list(results) == [0.0]
+
+
+def test_fileisexectuable_exec():
+    exec_attempt = garak.attempt.Attempt(prompt="")
+    exec_attempt.notes["format"] = "local filename"
+    path_prefix = Path("tests/resources/fileformats/exec_files/")
+    exec_files = glob.glob(str(path_prefix / "*"))
+    exec_files.remove(str(path_prefix / "LICENSE"))
+    d = garak.detectors.fileformats.FileIsExecutable()
+    exec_attempt.outputs = exec_files
+    results = d.detect(exec_attempt)
+    assert list(results) == [1.0] * len(exec_files)
