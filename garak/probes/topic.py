83e11db166d154802c2a6238479dc1f1a445df76
Leon Derczynski <leonderczynski@gmail.com>
2024-07-01 12:47:12 +0200

    disable topic probes by default (they need config); set up whitelisting checker

--- garak/probes/topic.py
+++ garak/probes/topic.py
@@ -7,14 +7,18 @@
 Attempt to get a model to engage on a number of contentious topics
 """
 
+from collections.abc import Iterable
+from typing import Iterable
 import wn
 
 from garak import _config
 from garak.probes.base import TreeSearchProbe
 
 
-class TopicExplorerWordnet(TreeSearchProbe):
-    """Use Wordnet to explore which topics a model will respond to or deflect"""
+class WordnetBlockedwords(TreeSearchProbe):
+    """Use Wordnet to explore which topics a model will respond to around blocked words"""
+
+    active = False
 
     bcp47 = "*"
     primary_detector = "mitigation.MitigationBypass"
@@ -72,3 +76,48 @@ class TopicExplorerWordnet(TreeSearchProbe):
 
         wn.download(self.lexicon)
         self.w = wn.Wordnet(self.lexicon)
+
+
+class WordnetAllowedwords(WordnetBlockedwords):
+    """Use Wordnet to find out if a model will discuss terms near but not in its allowed set"""
+
+    active = False  # only makes sense if a list is set
+
+    DEFAULT_PARAMS = WordnetBlockedwords.DEFAULT_PARAMS | {
+        #        "target_topics": ["banking"],
+        "target_topics": [],  # put list of allowed terms into this var
+        "lexicon": "oewn:2023",
+    }
+
+    def _get_node_parent(self, node):
+        return node.hypernyms()[0]  # wn is not always a DAG 🙃
+
+    def _get_node_siblings(self, node) -> Iterable:
+        siblings = set(self._get_node_children(self._get_node_parent(node)))
+        siblings.remove(node)
+        return siblings
+
+    def _get_initial_nodes(self):
+
+        allowed_nodes = []
+
+        for target_topic in self.target_topics:
+            allowed_nodes += self.w.synsets(target_topic)
+
+        self.never_queue = set(allowed_nodes)
+
+        neighbour_nodes = []
+        for allowed_node in allowed_nodes:
+            neighbour_nodes += self._get_node_siblings(allowed_node)
+
+        initial_nodes = [
+            node for node in neighbour_nodes if node not in self.never_queue
+        ]
+
+        if self.queue_children_at_start:
+            for synset in list(initial_nodes):
+                for hyponym in self._get_node_children(synset):
+                    if hyponym not in initial_nodes and hyponym not in self.never_queue:
+                        initial_nodes.append(hyponym)
+
+        return initial_nodes
