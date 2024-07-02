86bea07e7c31692e846e5bcb6e5c81cd8b149fbc
Leon Derczynski <leonderczynski@gmail.com>
2024-07-02 14:48:54 +0200

    permit multiple tree probes in log

--- garak/analyze/get_tree.py
+++ garak/analyze/get_tree.py
@@ -7,7 +7,8 @@ from collections import defaultdict
 import json
 import sys
 
-node_info = {}
+probes = set([])
+node_info = defaultdict(dict)
 
 with open(sys.argv[1], "r") as reportfile:
     for line in reportfile:
@@ -16,29 +17,36 @@ with open(sys.argv[1], "r") as reportfile:
             continue
         r = json.loads(line)
         if r["entry_type"] == "tree_data":
-            node_info[r["node_id"]] = r
-
-node_children = defaultdict(list)
-for node in node_info.values():
-    node_children[node["node_parent"]].append(node["node_id"])
-
-# roots: those with parents not in node_info, or none
-roots = set([])
-for node in node_info.values():
-    if node["node_parent"] is None or node["node_parent"] not in node_info.keys():
-        roots.add(node["node_id"])
+            probe = r["probe"]
+            probes.add(probe)
+            node_info[probe][r["node_id"]] = r
 
 
-def print_tree(node_id, indent=0):
-    forms = "" + ",".join(node_info[node_id]["surface_forms"]) + ""
-    print(
-        "  " * indent + f"{forms} ::> {node_info[node_id]['node_score']}",
-    )
-    children = node_children[node_id]
-    if children:
-        for child in children:
-            print_tree(child, indent + 1)
+for probe in probes:
+    print(f"============== {probe} ==============")
 
+    node_children = defaultdict(list)
+    for node in node_info[probe].values():
+        node_children[node["node_parent"]].append(node["node_id"])
 
-for root in sorted(list(roots)):
-    print_tree(root)
+    # roots: those with parents not in node_info, or none
+    roots = set([])
+    for node in node_info[probe].values():
+        if (
+            node["node_parent"] is None
+            or node["node_parent"] not in node_info[probe].keys()
+        ):
+            roots.add(node["node_id"])
+
+    def print_tree(node_id, indent=0):
+        forms = "" + ",".join(node_info[probe][node_id]["surface_forms"]) + ""
+        print(
+            "  " * indent + f"{forms} ::> {node_info[probe][node_id]['node_score']}",
+        )
+        children = node_children[node_id]
+        if children:
+            for child in children:
+                print_tree(child, indent + 1)
+
+    for root in sorted(list(roots)):
+        print_tree(root)
