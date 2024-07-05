18ea200e09e7392895b7a48c1200bab22c0904c9
Leon Derczynski <leonderczynski@gmail.com>
2024-07-05 15:03:44 +0200

    colour generation bar when filling up to #generations for each attempt

--- garak/generators/base.py
+++ garak/generators/base.py
@@ -11,6 +11,7 @@ import tqdm
 
 from garak import _config
 from garak.configurable import Configurable
+import garak.resources.theme
 
 
 class Generator(Configurable):
@@ -112,6 +113,7 @@ class Generator(Configurable):
 
         else:
             outputs = []
+
             if (
                 hasattr(_config.system, "parallel_requests")
                 and _config.system.parallel_requests
@@ -120,19 +122,25 @@ class Generator(Configurable):
             ):
                 from multiprocessing import Pool
 
-                bar = tqdm.tqdm(total=generations_this_call, leave=False)
-                bar.set_description(self.fullname[:55])
+                multi_generator_bar = tqdm.tqdm(
+                    total=generations_this_call,
+                    leave=False,
+                    colour=f"#{garak.resources.theme.GENERATOR_RGB}",
+                )
+                multi_generator_bar.set_description(self.fullname[:55])
 
                 with Pool(_config.system.parallel_requests) as pool:
                     for result in pool.imap_unordered(
                         self._call_model, [prompt] * generations_this_call
                     ):
                         outputs.append(result)
-                        bar.update(1)
+                        multi_generator_bar.update(1)
 
             else:
                 generation_iterator = tqdm.tqdm(
-                    list(range(generations_this_call)), leave=False
+                    list(range(generations_this_call)),
+                    leave=False,
+                    colour=f"#{garak.resources.theme.GENERATOR_RGB}",
                 )
                 generation_iterator.set_description(self.fullname[:55])
                 for i in generation_iterator:
