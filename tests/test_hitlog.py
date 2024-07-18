749203fb9c9b95e2c84a527dc5544d435e6026e8
Leon Derczynski <leonderczynski@gmail.com>
2024-07-18 10:27:03 +0200

    bump copyright

--- tests/test_hitlog.py
+++ tests/test_hitlog.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Portions Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Portions Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 import garak
