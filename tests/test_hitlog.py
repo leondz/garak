956bf29e221c3ed2223fe2f1a72bc2a8a3b9c7d5
Leon Derczynski <leonderczynski@gmail.com>
2024-07-18 10:22:43 +0200

    add test for general hitlog writing code path

---
+++ tests/test_hitlog.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: Portions Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+import garak
+import garak.cli
+
+
+def test_hitlog_codepath():
+
+    args = "-m test.Blank -p test.Test -d always.Fail".split()
+    garak.cli.main(args)
