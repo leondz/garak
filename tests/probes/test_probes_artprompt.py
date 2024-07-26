48f50ab98a750a3647e697c9d31beb0526e13814
Jeffrey Martin <jemartin@nvidia.com>
2024-07-26 09:14:06 -0500

    missing copyright header
    Signed-off-by: Jeffrey Martin <jemartin@nvidia.com>

--- tests/probes/test_probes_artprompt.py
+++ tests/probes/test_probes_artprompt.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python3
+# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
 
 import garak.probes.artprompt
 
