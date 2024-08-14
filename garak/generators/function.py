ac1966f5547635e8208a0ab36be10bee9fedd747
Soumili Nandi <soumilinandi99@gmail.com>
2024-08-13 22:56:34 -0700

    fix initialization bug for kwargs in function
    Signed-off-by: Soumili Nandi <soumilinandi99@gmail.com>

--- garak/generators/function.py
+++ garak/generators/function.py
@@ -53,7 +53,10 @@ from garak.generators.base import Generator
 class Single(Generator):
     """pass a module#function to be called as generator, with format function(prompt:str, **kwargs)->List[Union(str, None)] the parameter name `generations` is reserved"""
 
-    DEFAULT_PARAMS = {"generations": 10}
+    DEFAULT_PARAMS = {
+        "generations": 10,
+        "kwargs": {},
+    }
     doc_uri = "https://github.com/leondz/garak/issues/137"
     generator_family_name = "function"
     supports_multiple_generations = False
