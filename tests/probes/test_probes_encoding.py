e310baecfa3259ba6e9b534a655b0e30d086c14e
Jeffrey Martin <jemartin@nvidia.com>
2024-08-28 15:46:18 -0500

    unused imports and formatting
    Signed-off-by: Jeffrey Martin <jemartin@nvidia.com>

--- tests/probes/test_probes_encoding.py
+++ tests/probes/test_probes_encoding.py
@@ -1,8 +1,6 @@
 # SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-import pytest
-
 import garak.probes.encoding
 
 
