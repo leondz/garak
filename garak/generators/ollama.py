77057ee7f19bb7451d12b01043eace37f295c626
Martin <martin@pandiweb.dk>
2024-08-29 18:11:51 +0200

    Add a new OllamaGeneratorChat class, that uses the chat functionality

--- garak/generators/ollama.py
+++ garak/generators/ollama.py
@@ -16,7 +16,8 @@ class OllamaGenerator(Generator):
     """
 
     DEFAULT_PARAMS = Generator.DEFAULT_PARAMS | {
-        "timeout": 30 # Add a timeout of 30 seconds. Ollama can tend to hang forever, if this is not present
+        "timeout": 30, # Add a timeout of 30 seconds. Ollama can tend to hang forever on failures, if this is not present
+        "host": "127.0.0.1:11434" # The default host of an Ollama server. This should maybe be loaded from a config file somewhere
     }
 
     active = True
@@ -25,9 +26,27 @@ class OllamaGenerator(Generator):
     def __init__(self, name="", generations=10, config_root=_config):        
         super().__init__(name, generations, config_root) # Sets the name and generations
 
-        self.client = ollama.Client(timeout=self.DEFAULT_PARAMS["timeout"]) # Instantiates the client with the timeout
+        self.client = ollama.Client(self.DEFAULT_PARAMS['host'], timeout=self.DEFAULT_PARAMS["timeout"]) # Instantiates the client with the timeout
 
     @backoff.on_exception(backoff.fibo, (TimeoutError, ollama.ResponseError), max_value=70)
     def _call_model(self, prompt: str, generations_this_call: int = 1) -> List[Union[str, None]]:
         response = self.client.generate(self.name, prompt)
         return [response['response']]
+
+class OllamaGeneratorChat(OllamaGenerator):
+    """Interface for Ollama endpoints, using the chat functionality 
+
+        Model names can be passed in short form like "llama2" or specific versions or sizes like "gemma:7b" or "llama2:latest"
+    """
+
+    @backoff.on_exception(backoff.fibo, (TimeoutError, ollama.ResponseError), max_value=70)
+    def _call_model(self, prompt: str, generations_this_call: int = 1) -> List[Union[str, None]]:
+        response = self.client.chat(model=self.name, messages=[
+            {
+                'role': 'user',
+                'content': prompt,
+            },
+        ])
+        return [response['message']['content']]
+    
+DEFAULT_CLASS = "OllamaGeneratorChat"
