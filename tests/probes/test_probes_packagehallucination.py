3b97da973e0ff733c11de46e80b8197579140820
Arjun Krishna <arjunkrishna1306@gmail.com>
2024-08-30 14:21:25 -0400

    refactor package hallucination probe tests
    Signed-off-by: Arjun Krishna <arjunkrishna1306@gmail.com>

--- tests/probes/test_probes_packagehallucination.py
+++ tests/probes/test_probes_packagehallucination.py
@@ -5,20 +5,18 @@ import garak.probes.packagehallucination
 
 
 def test_promptcount():
-    p_python = garak.probes.packagehallucination.Python()
-    p_ruby = garak.probes.packagehallucination.Ruby()
-    p_javascript = garak.probes.packagehallucination.JavaScript()
+    languages = {
+        "Python": garak.probes.packagehallucination.Python(),
+        "Ruby": garak.probes.packagehallucination.Ruby(),
+        "JavaScript": garak.probes.packagehallucination.JavaScript(),
+    }
 
     expected_count = len(garak.probes.packagehallucination.stub_prompts) * len(
         garak.probes.packagehallucination.code_tasks
     )
 
-    assert (
-        len(p_python.prompts) == expected_count
-    ), f"Python prompt count mismatch. Expected {expected_count}, got {len(p_python.prompts)}"
-    assert (
-        len(p_ruby.prompts) == expected_count
-    ), f"Ruby prompt count mismatch. Expected {expected_count}, got {len(p_ruby.prompts)}"
-    assert (
-        len(p_javascript.prompts) == expected_count
-    ), f"JavaScript prompt count mismatch. Expected {expected_count}, got {len(p_javascript.prompts)}"
+    for language in languages:
+        language_probe = languages[language]
+        assert (
+            len(language_probe.prompts) == expected_count
+        ), f"{language} prompt count mismatch. Expected {expected_count}, got {len(language_probe.prompts)}"
