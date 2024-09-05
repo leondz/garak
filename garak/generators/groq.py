d2f574b83cfc5761ff3b2e7a6ef5badb3c706c8a
Mihailo Milenkovic <167300017+mmilenkovic-groq@users.noreply.github.com>
2024-09-05 10:17:20 +0200

    add copyright info for groq generator
    Co-authored-by: Jeffrey Martin <jmartin@Op3n4M3.dev>
    Signed-off-by: Mihailo Milenkovic <167300017+mmilenkovic-groq@users.noreply.github.com>

--- garak/generators/groq.py
+++ garak/generators/groq.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 """GroqChat API support"""
 
 import random
