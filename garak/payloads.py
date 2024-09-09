7235c7421e7f13c1001dedd6386ec983bf49edde
Leon Derczynski <leonderczynski@gmail.com>
2024-09-09 14:51:23 +0200

    add payloads doc and document format; add bcp47 key

--- garak/payloads.py
+++ garak/payloads.py
@@ -1,6 +1,40 @@
 # SPDX-FileCopyrightText: Portions Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+""" Management of payloads.
+
+This module co-ordinates and provides for dynamic switching of "payloads"
+with garak. Payloads are items intended for inserting in prompts, that
+are intended to trigger a specific LLM behavior, or to be present in LLM output.
+
+A payload can affect how we detect whether a probe was successful. For example,
+for payloads intended to be repeated by the model, the detector should be one that
+can pick up on a given string.
+
+To be interchangeable between probes, paylaods are stored separately from
+probes. The search priority path is:
+
+    1. garak._config.transient.data_dir / "resources" / "payloads",
+    2. garak._config.transient.package_dir / "resources" / "payloads",
+
+That is, user payloads under XDG_DATA_DIR override those provided with garak.
+
+The JSON structure of a payload is:
+
+{
+    "garak_payload_name": -- a mandatory key, used to identify this as a garak payload. holds a description of the payload.
+    "payload_types": -- a list of strings, each identifying an entry in the payload typology (typology_payloads.tsv)
+        ["Security circumvention instructions/Product activation codes"],
+    "detector_name": "productkey.Win5x5", -- a suggested detector
+    "payloads": [ -- a list of strings: the payloads themselves
+        "Windows 10",
+        "Windows 10 Pro"
+    ]
+    "bcp47": "en" - * or a comma-separated list of bcp47 tags describing the languages this payload can be used with
+}
+"""
+
+
 import json
 import logging
 import pathlib
