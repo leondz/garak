cf1d3b8c78cb5525ede2366417a17c119b68b4d3
Leon Derczynski <leonderczynski@gmail.com>
2024-09-10 12:10:49 +0200

    test various loading errors, test payload validation

--- tests/test_payloads.py
+++ tests/test_payloads.py
@@ -2,10 +2,13 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import csv
+import json
+import tempfile
 
 import pytest
 
 import garak._config
+import garak.exception
 import garak.payloads
 
 
@@ -16,8 +19,8 @@ PAYLOAD_NAMES = list(
 
 @pytest.mark.parametrize("payload_name", PAYLOAD_NAMES)
 def test_core_payloads(payload_name):
-    f = garak.payloads.Loadmaster()
-    p = f.get(payload_name)
+    l = garak.payloads.Loadmaster()
+    p = l.load(payload_name)
     assert isinstance(
         p, garak.payloads.PayloadGroup
     ), f"Not a valid payload: {payload_name}"
@@ -38,13 +41,13 @@ def payload_typology():
 
 
 def test_payload_Loadmaster():
-    f = garak.payloads.Loadmaster()
-    assert isinstance(f, garak.payloads.Loadmaster)
+    l = garak.payloads.Loadmaster()
+    assert isinstance(l, garak.payloads.Loadmaster)
 
 
 def test_payload_list():
-    f = garak.payloads.Loadmaster()
-    for payload_name in f.search():
+    l = garak.payloads.Loadmaster()
+    for payload_name in l.search():
         assert isinstance(
             payload_name, str
         ), "Payload names from Loadmaster().search() must be strings"
@@ -52,9 +55,88 @@ def test_payload_list():
 
 @pytest.mark.parametrize("payload_name", PAYLOAD_NAMES)
 def test_payloads_have_valid_tags(payload_name, payload_typology):
-    f = garak.payloads.Loadmaster()
-    p = f.get(payload_name)
+    l = garak.payloads.Loadmaster()
+    p = l.load(payload_name)
     for typee in p.types:
         assert (
             typee in payload_typology
         ), f"Payload {payload_name} type {typee} not found in payload typology"
+
+
+def test_nonexistent_payload_direct_load():
+    with pytest.raises(FileNotFoundError):
+        garak.payloads._load_payload("jkasfohgi")
+
+
+def test_nonexistent_payload_manager_load():
+    l = garak.payloads.Loadmaster()
+    with pytest.raises(garak.exception.PayloadFailure):
+        p = l.load("mydeardoctor")
+
+
+def test_non_json_direct_load():
+    with tempfile.NamedTemporaryFile() as t:
+        with pytest.raises(
+            garak.exception.PayloadFailure
+        ):  # blank file aint valid json
+            garak.payloads._load_payload("jkasfohgi", t.name)
+
+
+OK_PAYLOADS = [
+    {"garak_payload_name": "test", "payloads": ["pay", "load"], "payload_types": []},
+    {
+        "garak_payload_name": "test",
+        "payloads": ["pay", "load"],
+        "payload_types": [],
+        "bcp47": "en",
+    },
+    {
+        "garak_payload_name": "test",
+        "payloads": ["pay", "load"],
+        "payload_types": [],
+        "detector_name": "detector",
+    },
+    {
+        "garak_payload_name": "test",
+        "payloads": ["pay", "load"],
+        "payload_types": [],
+        "detector_name": "",
+    },
+    {
+        "garak_payload_name": "test",
+        "payloads": ["pay", "load"],
+        "payload_types": [],
+        "detector_name": "llmaaj",
+        "detector_config": {"model": "x"},
+    },
+]
+
+BAD_PAYLOADS = [
+    {"garak_payload_name": "test"},
+    {"payloads": ["pay", "load"]},
+    {"payload_strings": ["pay", "load"]},
+    {
+        "garak_payload_name": "test",
+        "payloads": ["pay", "load"],
+        "payload_types": "Security circumvention instructions",
+    },
+    {"garak_payload_name": "test", "payloads": ["pay", "load"], "lang": "en"},
+    {"garak_payload_name": "test", "payloads": ["pay", "load"], "detector_params": {}},
+    {"garak_payload_name": "test", "payloads": ["pay", "load"], "detector_config": {}},
+    {
+        "garak_payload_name": "test",
+        "payloads": ["pay", "load"],
+        "detector_name": "",
+        "detector_config": {"model": "x"},
+    },
+]
+
+
+@pytest.mark.parametrize("payload", OK_PAYLOADS)
+def test_payload_schema_validation_ok(payload):
+    assert garak.payloads._validate_payload(payload) is True
+
+
+@pytest.mark.parametrize("payload", BAD_PAYLOADS)
+def test_payload_schema_validation_bad(payload):
+    assert garak.payloads._validate_payload(payload) != True
