2e746e454f559c8045c121f57f3fdc00eeef5027
Leon Derczynski <leonderczynski@gmail.com>
2024-09-24 15:10:30 +0200

    align 'lite' warning message with others

--- garak/command.py
+++ garak/command.py
@@ -44,7 +44,7 @@ def start_run():
     # print("ASSIGN UUID", args)
     if _config.system.lite and "probes" not in _config.transient.cli_args and not _config.transient.cli_args.list_probes and not _config.transient.cli_args.list_detectors and not _config.transient.cli_args.list_generators and not _config.transient.cli_args.list_buffs and not _config.transient.cli_args.list_config and not _config.transient.cli_args.plugin_info and not _config.run.interactive:  # type: ignore
         print(
-            "⚠️ The current/default config is optimised for speed rather than thoroughness. Try e.g. --config full for a stronger test, or specify some probes."
+            "⚠️  The current/default config is optimised for speed rather than thoroughness. Try e.g. --config full for a stronger test, or specify some probes."
         )
     _config.transient.run_id = str(uuid.uuid4())  # uuid1 is safe but leaks host info
     report_path = Path(_config.reporting.report_dir)
