ca2e050a816d59018367c3f07b33d8fcfbb01e96
Jeffrey Martin <jemartin@nvidia.com>
2024-11-18 13:37:24 -0600

    additional tests for hugginface inference
    Add validation test to ensure uri values and names populate as expected
    
    Signed-off-by: Jeffrey Martin <jemartin@nvidia.com>

--- tests/generators/conftest.py
+++ tests/generators/conftest.py
@@ -11,3 +11,10 @@ def openai_compat_mocks():
     """Mock responses for OpenAI compatible endpoints"""
     with open(pathlib.Path(__file__).parents[0] / "openai.json") as mock_openai:
         return json.load(mock_openai)
+
+
+@pytest.fixture
+def hf_endpoint_mocks():
+    """Mock responses for Huggingface InferenceAPI based endpoints"""
+    with open(pathlib.Path(__file__).parents[0] / "hf_inference.json") as mock_openai:
+        return json.load(mock_openai)
