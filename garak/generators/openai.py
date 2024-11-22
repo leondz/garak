5c56469824b995940dca845c5c2f67ea18c387be
Jeffrey Martin <jemartin@nvidia.com>
2024-11-22 14:20:29 -0600

    Promote OpenAICompatible as first class generator
    Various generators extend this base template, usage and feedback
    suggest UX and usability improvements will be gained by promoting
    this class to be a `Configurable` generic OpenAI client based generator.
    
    * default `uri`
    * default implmentations of `_load_client()` and `_clear_client()`
    * remove duplicate versions of `_clear_client()`
    
    Signed-off-by: Jeffrey Martin <jemartin@nvidia.com>

--- garak/generators/openai.py
+++ garak/generators/openai.py
@@ -122,6 +122,7 @@ class OpenAICompatible(Generator):
     DEFAULT_PARAMS = Generator.DEFAULT_PARAMS | {
         "temperature": 0.7,
         "top_p": 1.0,
+        "uri": "http://localhost:8000/v1/",
         "frequency_penalty": 0.0,
         "presence_penalty": 0.0,
         "seed": None,
@@ -141,13 +142,16 @@ class OpenAICompatible(Generator):
         self._load_client()
 
     def _load_client(self):
-        # Required stub implemented when extending `OpenAICompatible`
-        # should populate self.generator with an openai api compliant object
-        raise NotImplementedError
+        self.client = openai.OpenAI(base_url=self.uri, api_key=self.api_key)
+        if self.name in ("", None):
+            raise ValueError(
+                f"{self.generator_family_name} requires model name to be set, e.g. --model_name org/private-model-name"
+            )
+        self.generator = self.client.chat.completions
 
     def _clear_client(self):
-        # Required stub implemented when extending `OpenAICompatible`
-        raise NotImplementedError
+        self.generator = None
+        self.client = None
 
     def _validate_config(self):
         pass
@@ -257,6 +261,11 @@ class OpenAIGenerator(OpenAICompatible):
     active = True
     generator_family_name = "OpenAI"
 
+    # remove uri as it is not overridable in this class.
+    DEFAULT_PARAMS = {
+        k: val for k, val in OpenAICompatible.DEFAULT_PARAMS.items() if k != "uri"
+    }
+
     def _load_client(self):
         self.client = openai.OpenAI(api_key=self.api_key)
 
@@ -288,10 +297,6 @@ class OpenAIGenerator(OpenAICompatible):
             msg = "'o1'-class models should use openai.OpenAIReasoningGenerator. Try e.g. `-m openai.OpenAIReasoningGenerator` instead of `-m openai`"
             logging.error(msg)
             raise garak.exception.BadGeneratorException("🛑 " + msg)
-
-    def _clear_client(self):
-        self.generator = None
-        self.client = None
 
     def __init__(self, name="", config_root=_config):
         self.name = name
